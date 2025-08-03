4805e595566308996c9f58189a3aac3752b555ac
jtmacoco <jtmacoco@gmail.com>
2025-08-03 00:19:31 -0700

    refactor/api-endpoints-central

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <div className='bg-black bg-full h-screen w-screen '>
-        <div className='mt-14 justify-center flex'>
+        <div className='p-10 justify-center flex'>
           <Form className='tex-white' action={"/search"}>
             <input className='mx-5 bg-gray-900' placeholder="type github url" name="query" />
             <button className='' type="submit">Submit</button>
--- frontend/src/constants/endpoints.tsx
+++ frontend/src/constants/endpoints.tsx
@@ -1,5 +1,5 @@
-class EndPoints{
-    static baseUrl = 'localhost:3000';
-    static codebase = 'codebase/';
-};
-export default EndPoints;
+export const API_BASE_URL = 'http://localhost:3000'
+export const API_ENDPOINTS = {
+    GIT_URL: `${API_BASE_URL}/codebase/gitUrl`
+}
+export default API_ENDPOINTS;
