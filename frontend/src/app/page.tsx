8c159c01617f95084e1c34d46d660656627ebef1
jtmacoco <jtmacoco@gmail.com>
2025-08-15 23:07:13 -0700

    fix: wait for handleSubmit to finish before navigating to chat page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,22 +2,31 @@
 import Form from 'next/form'
 import { useState } from 'react';
 import { useSubmitGitUrl } from '@/hooks/useSubmitGitUrl';
-import { Cal_Sans } from 'next/font/google';
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 export default function Home() {
-  const [url,setUrl] = useState('');
-  const {handleSubmit, error, isLoading} = useSubmitGitUrl();
+  const [url, setUrl] = useState('');
+  const { handleSubmit, error, isLoading } = useSubmitGitUrl();
+  const router = useRouter();
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const res = await handleSubmit(url);
+    if (res) {
+      router.push('/chatBot')
+    }
+  };
 
   return (
     <>
       <div className='bg-black bg-full h-screen w-screen '>
         <div className='p-10 justify-center flex'>
-          <Form className='tex-white' action={"/search"}>
-            <input value={url} onChange={(e)=>setUrl(e.target.value)} className='mx-5 bg-gray-900' placeholder="type github url" name="query" />
-            <Link href="/chatBot">
-            <button className='' type="submit" onClick={()=>handleSubmit(url)} disabled={isLoading}>Submit</button>
-          </Link>
-          </Form>
+          <form onSubmit={onSubmit} className='tex-white' action={"/search"}>
+            <input value={url} onChange={(e) => setUrl(e.target.value)} className='mx-5 bg-gray-900' placeholder="type github url" name="query" />
+            <button className='' type="submit"  disabled={isLoading}>
+              {isLoading ? 'Loading...' : 'Submit'}
+            </button>
+          </form>
         </div>
         {error && <p className='text-red-500'>{error}</p>}
       </div>
