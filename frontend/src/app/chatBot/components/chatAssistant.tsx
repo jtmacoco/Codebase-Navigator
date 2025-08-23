cee6ff81c872807d78905bca3ee57b85e5206084
jtmacoco <jtmacoco@gmail.com>
2025-08-23 01:20:40 -0700

    bug: fixed assistant response from overflowing text outside of bounds

--- frontend/src/app/chatBot/components/chatAssistant.tsx
+++ frontend/src/app/chatBot/components/chatAssistant.tsx
@@ -5,7 +5,7 @@ import remarkGfm from "remark-gfm"
 
 export default function ChatAssistant({ content }: { content: string }) {
     return (
-        <div className="prose prose-invert max-w-none">
+        <div className="prose prose-invert max-w-3xl">
             <ReactMarkdown remarkPlugins={[remarkGfm]}
             >
                 {content}
